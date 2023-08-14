5409964479e715c0f33fe6162498e3e490bb3f0c
K7 <klassenserver7bwin10@mail.com>
2023-08-14 12:38:15 +0200

    Some mvn version changes

--- src/test/java/de/klassenserver7b/danceinterpreter/AppTest.java
+++ src/test/java/de/klassenserver7b/danceinterpreter/AppTest.java
@@ -1,4 +1,4 @@
-package de.klassenserver7b.Danceinterpreter_newx;
+package de.klassenserver7b.danceinterpreter;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,32 +7,27 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 }
