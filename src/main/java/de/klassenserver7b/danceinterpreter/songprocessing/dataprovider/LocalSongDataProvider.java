428f9cdde4b5de1a1f2ccb73121e66cf86329c48
K7 <klassenserver7bwin10@mail.com>
2023-10-02 23:59:24 +0200

    Fixed Code Duplicate

--- src/main/java/de/klassenserver7b/danceinterpreter/songprocessing/dataprovider/LocalSongDataProvider.java
+++ src/main/java/de/klassenserver7b/danceinterpreter/songprocessing/dataprovider/LocalSongDataProvider.java
@@ -1,8 +1,6 @@
 
 package de.klassenserver7b.danceinterpreter.songprocessing.dataprovider;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
@@ -12,19 +10,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mpatric.mp3agic.ID3v2;
-import com.mpatric.mp3agic.InvalidDataException;
-import com.mpatric.mp3agic.Mp3File;
-import com.mpatric.mp3agic.UnsupportedTagException;
 import com.sun.nio.file.ExtendedOpenOption;
 
 import de.klassenserver7b.danceinterpreter.Main;
+import de.klassenserver7b.danceinterpreter.loader.FileLoader;
 import de.klassenserver7b.danceinterpreter.songprocessing.DanceInterpreter;
 import de.klassenserver7b.danceinterpreter.songprocessing.SongData;
 
@@ -61,57 +55,16 @@ public class LocalSongDataProvider implements SongDataProvider {
 		SongData ret = null;
 		DanceInterpreter danceI = Main.Instance.getDanceInterpreter();
 
-		Mp3File mp3file;
-		if (f != null) {
-			try {
-				mp3file = new Mp3File(f);
+		ret = new FileLoader().getDataFromFile(f);
 
-				if (mp3file.hasId3v2Tag()) {
-
-					ID3v2 tags = mp3file.getId3v2Tag();
-
-					String title = tags.getTitle();
-					String author = tags.getArtist();
-
-					byte[] imageData = tags.getAlbumImage();
-
-					BufferedImage img = new BufferedImage(1024, 1024, BufferedImage.TYPE_INT_ARGB);
-
-					// converting the bytes to an image
-					if (imageData != null) {
-						try {
-							img = ImageIO.read(new ByteArrayInputStream(imageData));
-						} catch (IOException e) {
-							this.log.error("Couldn't parse ID3 Cover");
-							log.error(e.getMessage(), e);
-						}
-					} else {
-						this.log.info("Song doesn't have a Cover");
-					}
-
-					Long length = mp3file.getLength();
-					String dance = danceI.getDance(title, author);
-
-					ret = new SongData(title, author, dance, length, img);
-
-					if (dance == null) {
-						danceI.addSongtoJSON(ret, "LOCAL");
-					}
-
-				} else {
-					ret = new SongData("Unknown", "Unknown", "null", 0L,
-							new BufferedImage(1024, 1024, BufferedImage.TYPE_INT_ARGB));
-				}
-
-			} catch (UnsupportedTagException | InvalidDataException | IOException e1) {
-				log.error(e1.getMessage(), e1);
-			}
+		if (ret.getDance() == null) {
+			danceI.addSongtoJSON(ret, "LOCAL");
 		}
 
 		if (ret == null || (datahash == ret.hashCode() && !provideforced)) {
 			return null;
 		}
-		
+
 		datahash = ret.hashCode();
 		return ret;
 
